22276dc73dd1de4a06902a1c72a54d80e1bd73ae
Meshabaz <shabazameen1818@gmail.com>
2022-11-22 12:58:24 +0530

    ock server

---
+++ index.js
@@ -0,0 +1,12 @@
+const data = require("./db.js");
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(data); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
