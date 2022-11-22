1d2559cdae54723184d6ea037a9ce439e7b3d89c
Meshabaz <shabazameen1818@gmail.com>
2022-11-22 18:47:40 +0530

    heroku

--- server.js
+++ server.js
@@ -10,3 +10,27 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+server.use(middlewares);
+
+// Add custom routes before JSON Server router
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// Use default router
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
