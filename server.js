a186d16faec5766d43328eb4907479749a7f5ab0
Meshabaz <shabazameen1818@gmail.com>
2022-11-23 18:23:37 +0530

    index work

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const data = require("./dummy.json");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(data); // <== Will be created later
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: "./index.html",
+});
 const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Server is running on port- ${port}`);
+});
 
 server.use(middlewares);
 
